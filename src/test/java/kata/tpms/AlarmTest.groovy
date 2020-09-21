bbb83224ebe5cdf787a3e3ff2af7ed7983bbfac1
Tony <huangyong@crossrainbow.cn>
2020-09-21 15:31:19 +0800

    Added TODO-new-feature: the alarm will be turned off before each checking of pressure

--- src/test/java/kata/tpms/AlarmTest.groovy
+++ src/test/java/kata/tpms/AlarmTest.groovy
@@ -5,8 +5,10 @@ import spock.lang.Specification
 
 class AlarmTest extends Specification{
     def a_trivial_test() {
-
         expect:
         5 == 2 +3
     }
+
+    // TODO-new-feature: the alarm will be turned off before each checking of pressure
+
 }
