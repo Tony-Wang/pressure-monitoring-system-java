dd9e0333464dd6a8420b1b3781ec05e54d9a4bbe
Tony <huangyong@crossrainbow.cn>
2020-09-21 16:12:15 +0800

    Added 3 acceptance test TODOs

--- src/test/java/kata/tpms/AlarmTest.groovy
+++ src/test/java/kata/tpms/AlarmTest.groovy
@@ -9,6 +9,9 @@ class AlarmTest extends Specification{
         5 == 2 +3
     }
 
+    // TODO-user-intent-test: a normal pressure value should not raise the alarm
+    // TODO-user-intent-test: a pressure value outside the range should raise the alarm
+    // TODO-user-intent-test: a normal pressure value after a value outside the range should not stop the alarm
     // TODO-new-feature: a normal pressure value after a value outside then range should stop the alarm
 
 }
